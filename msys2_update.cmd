a72b5374b9b4605922ae6d7b523227578201ef6a
Zachary Rose <68053187+ZSG-Studios@users.noreply.github.com>
2025-01-30 03:39:53 -0500

    Delete msys2_update.cmd

--- msys2_update.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-SET MSYS2_PATH=C:\msys64\msys2.exe
-SET MSYSTEM=MINGW64
-SET CHERE_INVOKING=1
-SET MSYS2_PATH_TYPE=inherit
-
-echo Running MSYS2 Update...
-start "" "%MSYS2_PATH%" /bin/bash -lc "/c/msys64/update_and_run.sh"
